2ba0dde280bc2c358c56ecdb91a6c3123daafd16
benaturalblue <yuuka.nakazaki513@gmail.com>
2023-05-17 21:33:37 +0900

    コードレビュー後の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]
+
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,16 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
     redirect_to root_path unless @item.user == current_user
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -37,6 +36,10 @@ class ItemsController < ApplicationController
   end
 
   private
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
 
   def item_params
     params.require(:item).permit(:image, :title, :content, :category_id, :condition_id, :charge_id, :area_id, :shipping_time_id,
