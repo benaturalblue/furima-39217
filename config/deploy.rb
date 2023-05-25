43e48e3ad0ddb98a02d31b19ad366c8e781256b3
benaturalblue <yuuka.nakazaki513@gmail.com>
2023-05-25 11:09:45 +0900

    自動デプロイの修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+#capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
 lock '3.17.3'
 
 # Capistranoのログの表示に利用する
@@ -6,6 +6,7 @@ set :application, 'furima-39217'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:benaturalblue/furima-39217.git'
+set :branch, 'main'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
